53d2069f6144586453cefdb957282d58e6e9b34a
Zain Khalid <muhammadzainkhalid08@gmail.com>
2020-02-09 14:50:40 +0500

    before making vue changes

--- resources/views/admin/welcome.blade.php
+++ resources/views/admin/welcome.blade.php
@@ -5,17 +5,25 @@
     <div class="container-fluid">
 
       <!-- Page Heading -->
-      <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Place Admin Instructions Here</h1>
+      <div class=" align-items-center justify-content-between mb-4">
+        <h1 class="h3 text-gray-800">Place Admin Instructions Here</h1>
+        <ul>
+          <li>ayah tag</li>
+            <ul>
+              <li>leave blank "" OR
+              <li>type full proper reference 'Al-Baqara:155'</li>
+            </ul>
+          <li>hadith tag</li>
+            <ul>
+              <li>leave blank "" OR
+              <li>type correct reference 'muslim' as anything typed in between quotes will be shown as is.</li>
+            </ul>
+          <li>tyme tag</li>
+            <ul>
+              <li>ensure to type BOTH min and sec. Can't skip.</li>
+            </ul>
+        </ul>
       </div>
     </div>
     <!-- /.container-fluid -->
-@endsection
-
-@section('page-script')
-  <!-- Page level plugins -->
-  <script src="{{ asset('admnistratorassetsonly/vendor/chart.js/Chart.min.js') }}"></script>
-
-  <!-- Page level custom scripts -->
-  <script src="{{ asset('js/demo/chart-area-demo.js') }}"></script>
 @endsection
