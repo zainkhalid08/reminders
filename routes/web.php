5998881dc65a94a316db2a8b90cb72a36d70217a
Zain Khalid <muhammadzainkhalid08@gmail.com>
2020-02-10 09:41:21 +0500

    caching with cache-control

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Post;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,18 +11,10 @@ use App\Post;
 |
 */
 
-// Route::get('/play', function(){
-// 	$post = Post::first();
-// 	echo dd($post->raw_content);
-// });
-
 Route::prefix(config('admin.slug'))->middleware('admin')->name('admin.')->group(function () {
 	Route::get('/', 'Auth\LoginController@showLoginForm')->name('login.view');
 	Route::post('login', 'Auth\LoginController@login')->name('login');
 	Route::post('logout', 'Auth\LoginController@logout')->name('logout');
-
-	// Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-	// Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
 
 	Route::get('dashboard', 'AdminController@dashboard')->name('dashboard');
 
@@ -38,16 +28,19 @@ Route::prefix(config('admin.slug'))->middleware('admin')->name('admin.')->group(
 	Route::put('post/{post}/publish', 'AdminPostPublisherController@publish')->name('post.update.publish');
 });
 
-Route::get('/', 'WelcomeController@welcome')->name('welcome')->middleware('cache.headers:public;max_age=36000'); // [1]
-// Route::get('/', 'WelcomeController@welcome')->name('welcome');
+Route::middleware(['cache.headers:public;max_age=36000'])->group(function () { // [1]
 
-Route::get('friday-sermons', 'PostController@index')->name('post.index'); 
+	Route::get('/', 'WelcomeController@welcome')->name('welcome'); 
+	Route::get('friday-sermons', 'PostController@index')->name('post.index'); 
 
-// title and location are kept optional just so the user can access different posts just by changing the number
-Route::get('friday-sermon/{post}/{title?}/{location?}', 'PostController@show')->name('post.show');
+	// title and location are kept optional just so the user can access different posts just by changing the number
+	Route::get('friday-sermon/{post}/{title?}/{location?}', 'PostController@show')->name('post.show');
 
-Route::get('/feedback', 'FeedbackController@create')->name('feedback');
+	Route::get('/feedback', 'FeedbackController@create')->name('feedback');
+});
+
 Route::post('/feedback', 'FeedbackController@store')->name('feedback.store');
+
 
 // REFERENCES: 
 // [1] : caching resource for 2678400s 1 month(31 days) https://developers.google.com/web/tools/lighthouse/audits/cache-policy?utm_source=lighthouse&utm_medium=devtools
