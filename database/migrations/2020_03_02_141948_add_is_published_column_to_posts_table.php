50591e24a9bdb09da0302f60dbf52937667a7b87
Zain Khalid <muhammadzainkhalid08@gmail.com>
2020-03-02 22:08:55 +0500

    just adding a is_published col

---
+++ database/migrations/2020_03_02_141948_add_is_published_column_to_posts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIsPublishedColumnToPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->boolean('is_published')->default(false)->after('user_id')->description('Is used to publish/unpublish a post.');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropColumn('is_published');
+        });
+    }
+}
