6e8e7f9be696f72313041e309b8d53f854aa0548
Zain Khalid <muhammadzainkhalid08@gmail.com>
2020-04-04 15:45:09 +0500

    defer removed from vue components script as it showed some ugly page before rendering the orignal one...ofcourse as we were defering the script execution until page is parsed

--- resources/views/partials/bottom_scripts.blade.php
+++ resources/views/partials/bottom_scripts.blade.php
@@ -1,4 +1,4 @@
 {{-- Jquery & Bootstrap & CleanBlog Js --}}
 <script src="{{ asset('js/vendor.js') }}" defer></script>
 {{-- Vue & its components --}}
-<script src="{{ asset('js/app.js') }}" defer></script>
+<script src="{{ asset('js/app.js') }}"></script>
