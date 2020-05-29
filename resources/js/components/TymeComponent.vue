df6cf71849caf3a7515dfc046ee89d3ea594db39
Zain Khalid <muhammadzainkhalid08@gmail.com>
2020-05-29 07:44:19 +0500

    tyme click to play in progress 80

--- resources/js/components/TymeComponent.vue
+++ resources/js/components/TymeComponent.vue
@@ -1,7 +1,7 @@
 <!-- This is used in post to tell at what time this piece of content is mentioned in the sermon eg. @1:44 -->
 <!-- usage as <tyme min="2" sec="4"></tyme> or <tyme min=2 sec=4></tyme> -->
 <template>
-    <small :title="makeTitle()">{{ showTime() }}</small>
+    <small :title="makeTitle()"><a href="" @click="playHere()">{{ showTime() }}</a></small>
 </template>
 
 <script>
@@ -14,6 +14,15 @@
             makeTitle() {
                 return 'Present at '+ this.minute + ' min(s) ' + this.second + ' sec(s) ' +'of the video shared on this page.';
             },
+            playHere() {
+                event.preventDefault(); // to prevent scroll to top behaviour
+                clearTimeout(timer);
+                if (isNaN(this.minute) || isNaN(this.second)) return
+                let seconds = (parseInt(this.minute) * 60) + parseInt(this.second);
+                player.seekTo(seconds, true);
+                player.playVideo();
+                timer = setTimeout(pauseVideo, 19000); // Note: it'll play for a little less than this time, coz of buffering
+            }
         },
         data() {
             return {
--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -31,7 +31,7 @@
     <div class="row">
       <div class="col-lg-8 col-md-10 mx-auto">
         <div class="embed-responsive embed-responsive-16by9">
-          <iframe width="560" height="315" src="{{ $post->video_src }}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <iframe id="funfyu" width="560" height="315" src="{{ $post->video_src }}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
         <span class="caption text-muted">© Rights of the audio belong to <a href="http://www.haramain.com/app" target="_blank" rel="noreferrer">Haramain Recordings</a></span>
         <div id="JwuWiw">
@@ -53,4 +53,24 @@
     </div>
   </div>
 </article>
+@endsection
+
+@section('bottom_scripts')
+<script>
+  var tag = document.createElement('script');
+  tag.src = "https://www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  var player; var timer;
+  function onYouTubeIframeAPIReady() {
+    player = new YT.Player('funfyu');
+  }
+  function pauseVideo() {
+    player.pauseVideo();
+  }
+</script>
+
+{{-- Vue & its components --}}
+<script src="{{ asset('js/app.js') }}"></script>
+
 @endsection
