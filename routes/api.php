afabfc8ac2701aaa8499cd8aa8251071c46eb3c1
Zain Khalid <muhammadzainkhalid08@gmail.com>
2020-07-09 15:48:57 +0500

    api for another project

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,12 @@ use Illuminate\Http\Request;
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+
+Route::get('/testendpoint2', function (Request $request) {
+    return [
+    	'data' => [
+    		['id'=>1, 'name'=>'loerore', 'category'=>['name'=>'cate']],
+    		['id'=>2, 'name'=>'srore', 'category'=>['name'=>'date2']]
+    	]
+    ];
+});
