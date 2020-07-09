fd45282b5b106d3b9015ac02cc7bca4e96faddbf
Zain Khalid <muhammadzainkhalid08@gmail.com>
2020-07-09 15:57:14 +0500

    cors try 2

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,11 @@ use Illuminate\Http\Request;
 // });
 
 Route::get('/testendpoint2', function (Request $request) {
+	header('Access-Control-Allow-Origin: *');
+	header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS, post, get');
+	header("Access-Control-Max-Age", "3600");
+	header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+	header("Access-Control-Allow-Credentials", "true");
     return [
     	'data' => [
     		['id'=>1, 'name'=>'loerore', 'category'=>['name'=>'cate']],
