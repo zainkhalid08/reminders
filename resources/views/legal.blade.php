a9ab0a56f6003a4e06960db4fbd98768c9aa8b2f
Zain Khalid <muhammadzainkhalid08@gmail.com>
2020-11-08 07:53:12 +0500

    gramitical fix on legal

--- resources/views/legal.blade.php
+++ resources/views/legal.blade.php
@@ -17,7 +17,7 @@
       <div class="col-lg-8 col-md-10 mx-auto">
         
         <h2>Privacy Policy</h2>
-        <p>We use <b>google analytics</b> to analyze traffic. With google analytics lots of technical data is collected such as browser information, operating system, cookies etc. The reason we use such third party services is to improve the overall user experience of our visitors and to know how well our blogs are performing. For more information on "How Google Uses Data" can be found <a class="turquoise" rel="noreferrer" target="_blank" href="https://www.google.com/policies/privacy/partners/">here.</a>
+        <p>We use <b>google analytics</b> to analyze traffic. With google analytics lots of technical data is collected such as browser information, operating system, cookies etc. The reason we use such third party services is to improve the overall user experience of our visitors and to know how well our blogs are performing. More information on "How Google Uses Data" can be found <a class="turquoise" rel="noreferrer" target="_blank" href="https://www.google.com/policies/privacy/partners/">here.</a>
 
         <div class="info">
           <p class="system-says"><strong>Last updated:</strong> 10<sup>th</sup> August, 2020</p>
