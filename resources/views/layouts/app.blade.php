8d062009d7ed8ec55093fe6caf783b071ff4cac8
Zain Khalid <muhammadzainkhalid08@gmail.com>
2021-03-14 14:31:54 +0500

    yandex

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,20 @@
       window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date()); gtag('config', 'UA-146593164-1');
     </script>
+    {{-- Yandex.Metrika counter --}}
+    <script type="text/javascript" >
+       (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+       m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+       (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+       ym(73738168, "init", {
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true,
+            webvisor:true
+       });
+    </script>
+    <noscript><div><img src="https://mc.yandex.ru/watch/73738168" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="theme-color" content="#317EFB"/>
--- resources/views/legal.blade.php
+++ resources/views/legal.blade.php
@@ -17,10 +17,10 @@
       <div class="col-lg-8 col-md-10 mx-auto">
         
         <h2>Privacy Policy</h2>
-        <p>We use <b>google analytics</b> to analyze traffic. With google analytics lots of technical data is collected such as browser information, operating system, cookies etc. The reason we use such third party services is to improve the overall user experience of our visitors and to know how well our blogs are performing. More information on "How Google Uses Data" can be found <a class="turquoise" rel="noreferrer" target="_blank" href="https://www.google.com/policies/privacy/partners/">here.</a>
+        <p>We use <b>google analytics</b> and <b>yandex metrica</b> to analyze traffic. Lots of technical data is collected such as browser information, operating system, cookies etc. The reason we use such third party services is to improve the overall user experience of our visitors and to know how well our blogs are performing. More information on "How Google Uses Data" can be found <a class="turquoise" rel="noreferrer" target="_blank" href="https://www.google.com/policies/privacy/partners/">here.</a>
 
         <div class="info">
-          <p class="system-says"><strong>Last updated:</strong> 10<sup>th</sup> August, 2020</p>
+          <p class="system-says"><strong>Last updated:</strong> 14<sup>th</sup> Febuary, 2021</p>
         </div>
 
       </div>
